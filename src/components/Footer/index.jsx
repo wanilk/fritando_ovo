436cc6e7a0edc7906757d740941b702697e7da27
Wanilk <wanilk@hotmail.com>
2023-02-28 22:16:10 -0400

    roda pé

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,9 +2,14 @@ import Style from "./Footer.module.css";
 
 export default function Footer() {
     return (
-        <footer className={Style.footer}>
-        <div className={Style.footer_content}>
-            <img src="../imagem/frigideiraRodape.svg" alt="" />
+        <footer  className={Style.footer}>
+        <div className={Style.footer_element}>
+            <img className={Style.footer_img} src="../imagem/frigideiraRodape.svg" alt="" />
+        
+        <div className={Style.footer_text}>
+            <h1>Fritando ovo - Receitas faceis</h1>
+            <h2>2022</h2>
+        </div>
         </div>
         </footer>
     );
